fa2d164e9e2ac1f466e32fed1a9705f6a28be3c3
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-06-14 15:19:33 +0600

    migration,model create done

---
+++ app/Serviecs/Admin/RackService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Serviecs\Admin;
+
+class RackService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
