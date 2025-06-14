d10191c4412597eeaebab42c3f23c50f4d0b8bca
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-06-14 18:27:54 +0600

    all over create

---
+++ app/Services/Admin/NewspaperService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Admin;
+
+class NewspaperService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
