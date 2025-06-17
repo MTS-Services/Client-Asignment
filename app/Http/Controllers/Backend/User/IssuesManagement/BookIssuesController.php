fafe5a00a13add8df3a6a3270a246ab01a2cb486
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-06-17 19:23:22 +0600

    User dashboard book issue dynamic done

---
+++ app/Http/Controllers/Backend/User/IssuesManagement/BookIssuesController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\Backend\User\IssuesManagement;
+
+use App\Http\Controllers\Controller;
+use App\Http\Traits\AuditRelationTraits;
+use App\Models\BookIssues;
+use App\Services\Admin\AdminManagement\AdminService;
+use App\Services\Admin\BookService;
+use App\Services\Admin\IssuesManagement\BookIssuesService;
+use App\Services\Admin\UserManagement\UserService;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
+use Yajra\DataTables\Facades\DataTables;
+
+class BookIssuesController extends Controller implements HasMiddleware
+{
+    use AuditRelationTraits;
+
+    protected function redirectIndex(): RedirectResponse
+    {
+        return redirect()->route('user.book-issues-list');
+    }
+
+    protected BookIssuesService $bookIssuesService;
+    protected UserService $userService;
+    protected BookService $bookService;
+    protected AdminService $adminService;
+
+
+    public function __construct(BookIssuesService $bookIssuesService, UserService $userService, BookService $bookService, AdminService $adminService)
+    {
+        $this->bookIssuesService = $bookIssuesService;
+        $this->userService = $userService;
+        $this->bookService = $bookService;
+        $this->adminService = $adminService;
+    }
+
+    public static function middleware(): array
+    {
+        return [
+            'auth:admin', // Applies 'auth:admin' to all methods
+
+            // Permission middlewares using the Middleware class
+            new Middleware('permission:book-issues-list', only: ['issuesList']),
+            new Middleware('permission:book-issues-details', only: ['issueDetails']),
+            //add more permissions if needed
+        ];
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function issuesList(Request $request)
+    {
+        $status = $request->get('status');
+        if ($request->ajax()) {
+            $query = $this->bookIssuesService->getBookIssuess()->where('status', array_search($status, BookIssues::statusList()))->self();
+
+            return DataTables::eloquent($query)
+                ->editColumn('user_id', fn($bookIssues) => $bookIssues->user?->name)
+                ->editColumn('book_id', fn($bookIssues) => $bookIssues->book?->title)
+                ->editColumn('status', fn($bookIssues) => "<span class='badge badge-soft {$bookIssues->status_color}'>{$bookIssues->status_label}</span>")
+                ->editColumn('created_at', fn($bookIssues) => $bookIssues->created_at_formatted)
+                ->editColumn('action', fn($bookIssues) => view('components.user.action-buttons', [
+                    'menuItems' => $this->menuItems($bookIssues, $status, $request)
+                ])->render())
+                ->rawColumns(['created_by', 'issued_by', 'returned_by', 'user_id', 'book_id', 'status', 'creater_id', 'action'])
+                ->make(true);
+        }
+
+        return view('backend.user.book-issues.index', compact('status'));
+    }
+
+
+    protected function menuItems($model, $status): array
+    {
+        return [
+            [
+                'routeName' => 'user.book-issues-show',
+                'params' => ['status' => $status],
+                'className' => 'view',
+                'label' => 'Details',
+                'permissions' => ['permission-list']
+            ],
+        ];
+    }
+    public function issuesShow(Request $request, string $status)
+    {
+        $book_issue = $this->bookIssuesService->getBookIssues($status, 'status');
+        $book_issue['username'] = $book_issue->user?->name;
+        $book_issue['bookTitle'] = $book_issue->book?->title;
+        $book_issue['issuedBy'] = $book_issue->issuedBy?->name;
+        $book_issue['returnedBy'] = $book_issue->returnedBy?->name;
+        $book_issue['creater_name'] = $this->creater_name($book_issue);
+        $book_issue['updater_name'] = $this->updater_name($book_issue);
+        return view('backend.user.book-issues.show', compact('book_issue'));
+    }
+}
---
+++ resources/views/backend/user/book-issues/show.blade.php
@@ -0,0 +1,35 @@
+<x-user::layout>
+    <x-slot name="title">{{ __('Book Issues List') }}</x-slot>
+    <x-slot name="breadcrumb">{{ __('Book Issues List') }}</x-slot>
+    <x-slot name="page_slug">book_issues_{{ request('status') }}</x-slot>
+    <section>
+        <div class="glass-card rounded-2xl py-6">
+            <div class="w-full">
+                <!-- Header Section -->
+                <div class="flex items-center justify-between">
+                    <div class="mb-2 ps-6">
+                        <h1 class="text-base md:text-lg xl:text-xl font-bold text-gray-800 dark:text-white mb-2">
+                            {{ $book_issue->book?->title }}
+                        </h1>
+                        <p class="text-sm text-gray-500 dark:text-gray-400">
+                            {{ __('Published on') }} {{ $book_issue->created_at_formatted }}
+                        </p>
+                    </div>
+                    <div class="flex items-center gap-2 pe-6">
+                        <x-user.primary-link href="{{ route('user.book-issues-list', ['status' => request('status')]) }}">{{ __('Back') }} <i
+                                data-lucide="undo-2" class="w-4 h-4"></i>
+                        </x-user.primary-link>
+                    </div>
+                </div>
+
+                <div class="h-px bg-gray-300 dark:bg-gray-700 mb-6"></div>
+
+                <!-- Details Card -->
+                <div class="px-6">
+                    <p class="text-text-light-primary dark:text-text-dark-primary">{{ $book_issue->notes }}</p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+</x-user::layout>
