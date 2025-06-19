4fce5a8d0e996f965ef2ea2bb0d3e0ac86929a32
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-06-19 18:00:33 +0600

    add permission to component button

--- resources/views/backend/admin/admin-management/admin/index.blade.php
+++ resources/views/backend/admin/admin-management/admin/index.blade.php
@@ -8,10 +8,10 @@
             <div class="flex items-center justify-between">
                 <h2 class="text-xl font-bold text-text-black dark:text-text-white">{{ __('Admin List') }}</h2>
                 <div class="flex items-center gap-2">
-                    <x-admin.primary-link secondary="true" href="{{ route('am.admin.trash') }}">{{ __('Trash') }} <i
+                    <x-admin.primary-link secondary="true" href="{{ route('am.admin.trash') }}" permission="admin-trash">{{ __('Trash') }} <i
                             data-lucide="trash-2" class="w-4 h-4"></i>
                     </x-admin.primary-link>
-                    <x-admin.primary-link href="{{ route('am.admin.create') }}">{{ __('Add') }} <i
+                    <x-admin.primary-link href="{{ route('am.admin.create') }}" permission="admin-create">{{ __('Add') }} <i
                             data-lucide="user-round-plus" class="w-4 h-4"></i>
                     </x-admin.primary-link>
                 </div>
