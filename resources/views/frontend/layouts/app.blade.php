01f2549e983ef0a4e2985d6cdeb66d99415bc7de
Md Shariful Islam <devshariful99@gmail.com>
2025-06-21 15:37:14 +0600

    Update app.blade.php

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="{{ env('FAVICON') }}" type="image/x-icon">
 
     <title>
         {{ isset($title) ? $title . ' - ' : '' }}
@@ -55,7 +56,7 @@
     </script>
 
     @stack('css')
-    {{-- 
+    {{--
     <style>
         .bg-animated {
             background-color: hsla(0, 100%, 50%, 1);
